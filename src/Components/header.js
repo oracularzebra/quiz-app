5e0e11130873a72fd5efcc38f6a5ff344cf90fdb
Kartikey Gupta <Oracular@localhost.localdomain>
2022-07-17 19:50:28 +0530

    Added the header file

---
+++ src/Components/header.js
@@ -0,0 +1,13 @@
+const Header = ()=>{
+
+    return (
+        <div className="bg-blue-500 flex w-5 h-5/6">
+            <header>
+                <h1>
+                    Quizze
+                </h1>
+            </header>
+        </div>
+    )
+}
+export default Header;
