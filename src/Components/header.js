3f30898bc944400281df9741ace364eedf9f965b
Kartikey Gupta <Oracular@localhost.localdomain>
2022-07-17 21:38:07 +0530

    Added the text

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,11 +1,14 @@
 const Header = ()=>{
 
     return (
-        <div className="bg-blue-500 flex w-5 h-5/6">
+        <div className="bg-blue-500 w-screen h-full">
             <header>
-                <h1>
-                    Quizze
+                <h1 className="font-semibold text-center text-4xl">
+                    Quizee
                 </h1>
+                <p className="text-center">
+                    An trivial knowledge assessment website
+                </p>
             </header>
         </div>
     )
