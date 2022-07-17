cb39979e61639c4b47fdb92ad0eb10f08ffb731f
Kartikey Gupta <Oracular@localhost.localdomain>
2022-07-17 22:17:47 +0530

    Adding cards in home page, and added the border in header

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,7 +1,7 @@
 const Header = ()=>{
 
     return (
-        <div className="bg-blue-500 w-screen">
+        <div className="bg-blue-500 w-screen rounded-b-md">
             <header>
                 <h1 className="font-semibold text-center text-4xl">
                     Quizee
--- src/Components/home.js
+++ src/Components/home.js
@@ -1,6 +1,6 @@
-const Home = ()=>{
-    <div>
-
+const Home = ()=>(
+    <div className="bg-red-300">
+        Here we will have sections, containing abstract cards
     </div>
-}
+    )
 export default Home;
