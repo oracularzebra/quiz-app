660bb0e98cff9d02d276ebf1cea3789cd81be783
Kartikey Gupta <Oracular@localhost.localdomain>
2022-07-18 17:35:39 +0530

    Added TitleCard page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "./Components/footer";
 import Header from "./Components/header";
 import Home from "./Components/home";
+import TitleCard from "./Components/AbstractCard";
 
 function App() {
   return (
-    <div className="grid h-full ">
+    <div className="grid h-full bg-slate-300">
       <Header/>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
+        <Route path="/:categoryName" element={<TitleCard/>}></Route>
       </Routes>
       <Footer/>
     </div>
