21bc35739e175e3850a30da0708d7d39d66ca24d
Kartikey Gupta <Oracular@localhost.localdomain>
2022-07-19 22:12:05 +0530

    Assured about the working of the useParams

--- src/Components/AbstractCard.js
+++ src/Components/AbstractCard.js
@@ -1,12 +1,17 @@
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const TitleCard = ()=>{
 
-    const {categoryName} = useParams();
+    const {categoryName, difficultyLevel} = useParams();
+    const navigate = useNavigate();
+    console.log(categoryName, difficultyLevel);
+
+    
 
     return (
         <div>
-            <h1 className="text-xl">{categoryName}</h1>
+            <h1 className="text-xl">{categoryName+ difficultyLevel}</h1>
         </div>
     )
 }
