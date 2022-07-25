d7eb634323c0908dc03ecacdd4b75fd6f2e2d29b
Kartikey Gupta <Oracular@localhost.localdomain>
2022-07-25 11:14:52 +0530

    replaced the object with only parameters

--- src/Components/getQuestions.js
+++ src/Components/getQuestions.js
@@ -1,8 +1,11 @@
-const getQuestions = ({category, difficulty_level})=>{
+const getQuestions = (category, difficulty_level)=>{
 
+    console.log(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty_level}`);
     return fetch(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty_level}`).
         then(response => response.json()).
-        then(responseJson => responseJson).
+        then(responseJson => {
+            console.log(responseJson);
+            return responseJson}).
         catch(error => console.log(error));
 }
 export default getQuestions;
