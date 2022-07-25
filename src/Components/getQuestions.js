3552de4d23503c783653c44f6fa9470602ddce2e
Kartikey Gupta <Oracular@localhost.localdomain>
2022-07-25 10:54:34 +0530

    Implemented the request to get questions

--- src/Components/getQuestions.js
+++ src/Components/getQuestions.js
@@ -1,6 +1,6 @@
 const getQuestions = ({category, difficulty_level})=>{
 
-    return fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy').
+    return fetch(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty_level}`).
         then(response => response.json()).
         then(responseJson => responseJson).
         catch(error => console.log(error));
