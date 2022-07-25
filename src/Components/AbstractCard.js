0f3be84658d1c7a719b73df2ff5639621277fdc0
Kartikey Gupta <Oracular@localhost.localdomain>
2022-07-25 11:18:54 +0530

    Implemented the useEffect for questin fetching, now all left for implementing is the interface

--- src/Components/AbstractCard.js
+++ src/Components/AbstractCard.js
@@ -1,17 +1,24 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import getQuestions from "./getQuestions";
 
 const TitleCard = ()=>{
 
-    const {categoryName, difficultyLevel} = useParams();
-    const navigate = useNavigate();
-    console.log(categoryName, difficultyLevel);
-
-    
+    const {category, difficultyLevel} = useParams();
+    console.log(category, difficultyLevel);
+    const [questions, setQuestions] = useState(null);
+    useEffect(()=>{
+        const getQues = async()=>{
+            const ques = await getQuestions(category, difficultyLevel);
+            console.log(ques);
+            setQuestions(ques);
+        }
+        getQues();
+    }, [category, difficultyLevel]);
 
     return (
         <div>
-            <h1 className="text-xl">{categoryName+ difficultyLevel}</h1>
+            <h1 className="text-xl">{category+ difficultyLevel}</h1>
         </div>
     )
 }
