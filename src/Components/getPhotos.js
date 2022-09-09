a12ac07fd6c40db8a6cf979f379669f99b7efeb4
oracular <kartikeykumar150@gmail.com>
2022-09-09 17:05:01 +0530

    Added the photo getting code from pixels

---
+++ src/Components/getPhotos.js
@@ -0,0 +1,21 @@
+async function getPhoto(query, quantity=1){
+
+    const data=await fetch(`https://api.pexels.com/v1/search?query=${query}&page=1&per_page=${quantity}`, 
+    {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: '563492ad6f917000010000017d09a555ba2540eda58754fc2ce5c733',
+        },
+    });
+
+    if(data.ok){
+        const response = await data.json();
+        return response;
+    }
+    else{
+        console.log('Limit exceeded')
+    }
+}
+
+export default getPhoto;
