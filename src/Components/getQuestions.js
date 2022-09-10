2aaedaa38e957d7f669f932be0d14b297faacc90
oracular <kartikeykumar150@gmail.com>
2022-09-10 12:50:27 +0530

    Added the url encode

--- src/Components/getQuestions.js
+++ src/Components/getQuestions.js
@@ -1,11 +1,9 @@
 const getQuestions = (category, difficulty_level)=>{
 
-    console.log(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty_level}`);
-    return fetch(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty_level}`).
+    return fetch(`https://opentdb.com/api.php?amount=10&encode=url3986&category=${category}&difficulty=${difficulty_level}`).
         then(response => response.json()).
         then(responseJson => {
-            console.log(responseJson);
-            return responseJson}).
+             return responseJson}).
         catch(error => console.log(error));
 }
 export default getQuestions;
