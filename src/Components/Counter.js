3a58b6ee5db7cd4aca06b116752a86dd98f0d8cd
oracular <kartikeykumar150@gmail.com>
2022-09-11 15:13:47 +0530

    Added the timer

---
+++ src/Components/Counter.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+const Counter=({testTime})=>{
+
+    const [counter, setCounter] = useState(testTime);
+
+    function tick(){
+
+        if(counter.min === 0 && counter.sec === 0){
+            //show the result
+        }
+        else if(counter.sec == 0){
+            setCounter({min:counter.min-1, sec:counter.sec});
+        }
+        else{
+            setCounter({min:counter.min, sec:counter.sec-1});
+        }
+    }
+    useEffect(()=>{
+        setTimeout(()=>{
+            tick();
+        }, 1000);
+    });
+
+    return (
+        <div>
+            {counter.min+":"+ counter.sec}
+        </div>
+    )
+}
+export default Counter;
