fcbc24a4aa2f02209923607c82e7346583b61375
oracular <kartikeykumar150@gmail.com>
2022-09-11 19:52:30 +0530

    Set the width of clickable are to the half width

--- src/Components/QuestionPage/NextAndPrevButtons.js
+++ src/Components/QuestionPage/NextAndPrevButtons.js
@@ -2,8 +2,8 @@ const NextPrevBtn = ({currQuesIndex, setCurrQuesIndex, questions})=>{
 
     return (
         <div className="flex p-3 rounded-md justify-around row-start-3 bg-slate-200 row-end-3 col-start-2 col-end-3">
-            <button onClick={()=>{if(currQuesIndex !== 0)setCurrQuesIndex(currQuesIndex-1); else setCurrQuesIndex(questions.length-1);}}>Prev</button>
-            <button onClick={()=>{if(currQuesIndex !== questions.length-1)setCurrQuesIndex(currQuesIndex+1); else setCurrQuesIndex(0);}}>Next</button>
+            <button className="w-full" onClick={()=>{if(currQuesIndex !== 0)setCurrQuesIndex(currQuesIndex-1); else setCurrQuesIndex(questions.length-1);}}>Prev</button>
+            <button className="w-full" onClick={()=>{if(currQuesIndex !== questions.length-1)setCurrQuesIndex(currQuesIndex+1); else setCurrQuesIndex(0);}}>Next</button>
         </div>
     )
 
