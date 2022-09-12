ea60bc3665058d1ef3d17fec551a88ea6b8b7ff0
oracular <kartikeykumar150@gmail.com>
2022-09-12 19:48:54 +0530

    Changed the background color of the option titles and added the reset button

--- src/Components/QuestionPage/Options.js
+++ src/Components/QuestionPage/Options.js
@@ -1,19 +1,20 @@
 const Options = ({currOptions, setMarkedOptions,quesIndex, markedOptions})=>{
+
     return (
-        <div className="bg-blue-300 roundebg-inheritd-xl grid m-2 col-start-2 col-end-3 row-start-2 row-end-3">
+        <div className="flex flex-col m-2 col-start-2 col-end-3 row-start-2 row-end-3">
             {currOptions.map((option, index)=>(
-                <label key={index}>
-                <input type={"radio"} checked={markedOptions[quesIndex]===decodeURIComponent(option)?true:false} className="m-2" onChange={(event)=>{
+                <label className="rounded-md m-2 bg-slate-200" key={index}>
+                <input type={"radio"} checked={markedOptions[quesIndex]===decodeURIComponent(option)?true:false} className="m-2 scale-110" onChange={(event)=>{
                     event.currentTarget.checked = true;
                     const newMarkedOptions = markedOptions.map((_, index)=>{
                         if(index === quesIndex) return decodeURIComponent(option);
                         return decodeURIComponent(markedOptions[index]);
                     })
-                    console.log(newMarkedOptions);
                     setMarkedOptions(newMarkedOptions);
                 }}/>{decodeURIComponent(option)}
                 </label>
             ))}
+            <button className="font-semibold" onClick={()=>setMarkedOptions([...markedOptions.slice(0, quesIndex), "undefined", ...markedOptions.slice(quesIndex+1)])}>Reset</button>
         </div>
     )
 }
