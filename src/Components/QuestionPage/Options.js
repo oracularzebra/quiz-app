7120b29cf07fe862e4449c6fd3271a8f776c09a4
oracular <kartikeykumar150@gmail.com>
2022-09-12 19:20:45 +0530

    Handled the checkbox for options

--- src/Components/QuestionPage/Options.js
+++ src/Components/QuestionPage/Options.js
@@ -1,9 +1,18 @@
-const Options = ({currOptions})=>{
-
+const Options = ({currOptions, setMarkedOptions,quesIndex, markedOptions})=>{
     return (
-        <div className="bg-blue-300 rounded-xl m-2 col-start-2 col-end-3 row-start-2 row-end-3 grid justify-around items-center content-center">
+        <div className="bg-blue-300 roundebg-inheritd-xl grid m-2 col-start-2 col-end-3 row-start-2 row-end-3">
             {currOptions.map((option, index)=>(
-                <button className="m-2" key={index}>{decodeURIComponent(option)}</button>
+                <label key={index}>
+                <input type={"radio"} checked={markedOptions[quesIndex]===decodeURIComponent(option)?true:false} className="m-2" onChange={(event)=>{
+                    event.currentTarget.checked = true;
+                    const newMarkedOptions = markedOptions.map((_, index)=>{
+                        if(index === quesIndex) return decodeURIComponent(option);
+                        return decodeURIComponent(markedOptions[index]);
+                    })
+                    console.log(newMarkedOptions);
+                    setMarkedOptions(newMarkedOptions);
+                }}/>{decodeURIComponent(option)}
+                </label>
             ))}
         </div>
     )
