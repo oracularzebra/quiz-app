dce77657ccbc40f910eb593cd1c95bb471b3bd04
oracular <kartikeykumar150@gmail.com>
2022-09-13 18:11:32 +0530

    Removed the decodeURIComponent

--- src/Components/QuestionPage/Options.js
+++ src/Components/QuestionPage/Options.js
@@ -4,14 +4,14 @@ const Options = ({currOptions, setMarkedOptions,quesIndex, markedOptions})=>{
         <div className="flex flex-col m-2 col-start-2 col-end-3 row-start-2 row-end-3">
             {currOptions.map((option, index)=>(
                 <label className="rounded-md m-2 bg-slate-200" key={index}>
-                <input type={"radio"} checked={markedOptions[quesIndex]===decodeURIComponent(option)?true:false} className="m-2 scale-110" onChange={(event)=>{
+                <input type={"radio"} checked={markedOptions[quesIndex]===option?true:false} className="m-2 scale-110" onChange={(event)=>{
                     event.currentTarget.checked = true;
                     const newMarkedOptions = markedOptions.map((_, index)=>{
-                        if(index === quesIndex) return decodeURIComponent(option);
-                        return decodeURIComponent(markedOptions[index]);
+                        if(index === quesIndex) return option;
+                        return markedOptions[index];
                     })
                     setMarkedOptions(newMarkedOptions);
-                }}/>{decodeURIComponent(option)}
+                }}/>{option}
                 </label>
             ))}
             <button className="font-semibold" onClick={()=>setMarkedOptions([...markedOptions.slice(0, quesIndex), "undefined", ...markedOptions.slice(quesIndex+1)])}>Reset</button>
