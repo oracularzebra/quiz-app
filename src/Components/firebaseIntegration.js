93c88230ffd641c73a36bbf1b9d42ade21a4a5c7
oracular <kartikeykumar150@gmail.com>
2022-09-13 22:48:46 +0530

    Added the databaseurl

--- src/Components/firebaseIntegration.js
+++ src/Components/firebaseIntegration.js
@@ -10,8 +10,11 @@ const firebaseConfig = {
   projectId: "quizze-3dd02",
   storageBucket: "quizze-3dd02.appspot.com",
   messagingSenderId: "227820419143",
-  appId: "1:227820419143:web:3b78185a827ef65e8e1962"
+  appId: "1:227820419143:web:3b78185a827ef65e8e1962",
+  databaseURL: "https://quizze-3dd02-default-rtdb.firebaseio.com",
+
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export default app;
