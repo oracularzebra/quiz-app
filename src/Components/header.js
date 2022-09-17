f094dcd74655f4fa73acaa3a4ac3213896675bbd
oracular <kartikeykumar150@gmail.com>
2022-09-18 00:11:17 +0530

    Made the header heading a link to the home page and removed its underilne and blue color

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../index.css"
 
 const Header = ()=>{
@@ -6,9 +7,9 @@ const Header = ()=>{
     return (
         <div className="header">
             <header>
-                <h1>
-                    Quizee
-                </h1>
+                <Link to={'/home'}>
+                    <h1>Quizee</h1>
+                </Link>
                 <p>
                     An trivial knowledge assessment website
                 </p>
